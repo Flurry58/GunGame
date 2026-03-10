224123651775dc2650de60f65a17fa310ddb1447
Logan <loganc556@gmail.com>
2026-03-10 10:48:58 -0400

    Added Health Box to restore health on impact

--- gun-game-v2/Enemy/TestEnemy/enemy_test.gd
+++ gun-game-v2/Enemy/TestEnemy/enemy_test.gd
@@ -5,13 +5,13 @@ signal update_health_bar(amount)
 signal Do_Damage(data: Dictionary)
 
 
-
 func _on_mediator_child_output(child_name: String, data: Dictionary) -> void:
 	if child_name == "Health" and data.has("Current"):
 		if data["Current"] == 0:
 			self.queue_free()
 
-
+# Connect to mediator, send mediator health change signal, disconnect from mediator to preserve
+# memory integrity
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	Do_Damage.connect(body.Mediator.process_signal)
 	Do_Damage.emit("Health Component", {"Change":Damage_Amount})
@@ -19,5 +19,3 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 func _physics_process(delta: float) -> void:
 	var output = Vector2.RIGHT.rotated(round(velocity.angle() / TAU * 4) * TAU / 4).snapped(Vector2.ONE)
 	send_velocity.emit(output)
-
-	
